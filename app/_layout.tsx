71b9bee6a0e399bbaa7944a1cf23ecc97d3f4946
Raj Thakur <rajthakur2003@gmail.com>
2025-01-17 10:44:00 +0000

    trying to make google auth work

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import "./global.css";
 
 import {useFonts} from "expo-font"
 import { useEffect } from "react";
+import GlobalProvider from "@/lib/global-provider";
 
 export default function RootLayout() {
 
@@ -23,5 +24,8 @@ export default function RootLayout() {
   
   if(!fontsLoaded) return null;
 
-  return <Stack screenOptions={{headerShown: false}}/>;
+  return (
+    <GlobalProvider>
+  <Stack screenOptions={{headerShown: false}}/>
+  </GlobalProvider>);
 }
